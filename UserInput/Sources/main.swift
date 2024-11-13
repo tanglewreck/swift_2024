364dfd34dad991ce0150841ed62760f4903ad03a
mier <mier@localhost>
2024-11-13 12:41:15 +0100

    Testing 'readLine()'

---
+++ UserInput/Sources/main.swift
@@ -0,0 +1,6 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+print("give me some input", terminator: " ")
+let myInput: String = readLine() // ?? "(nothing typed)"
+print(myInput)
