6978be194fd0e4eb19eed812f06bb17e1855e507
mier <mier@localhost>
2024-11-13 13:30:37 +0100

    testing various ways to unwrap strings and numerics

--- UserInput/Sources/main.swift
+++ UserInput/Sources/main.swift
@@ -1,6 +1,25 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-print("give me some input", terminator: " ")
-let myInput: String = readLine() // ?? "(nothing typed)"
-print(myInput)
+print("give me a number", terminator: ": ")
+// let myOptStringNum = readLine()
+// let myString = readLine()!
+// let myString = myOptStringNum!
+// let myNum1 = Int(myOptStringNum!)!
+// let myNum2 = Int(myString)!
+// print(myString)
+// print(myNum1 * 42)
+// var myNum = Int(readLine()!)!
+var myFloat = Float(readLine()!)!
+// print(myFloat * 42)
+var myProduct = myFloat * 42
+print("\(myFloat) * 42 = \(myProduct)")
+let myMsg = "product of \(myFloat) * 42 is "
+    if myProduct > 100 {
+        print(myMsg + "larger than 100.0")
+    } else if myProduct > 50 {
+        print(myMsg + "larger than 50.0")
+    } else {
+        print(myMsg + "less than 50.0")
+    }
+// print(Int(myNum! * 42))
