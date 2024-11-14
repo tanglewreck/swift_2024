65acfd0981046544ce9a1293cc4278e60e38b4ce
mier <mier@localhost>
2024-11-14 14:22:18 +0100

    Just a reordering of some lines

--- UserInput/Sources/main.swift
+++ UserInput/Sources/main.swift
@@ -2,15 +2,17 @@
 // https://docs.swift.org/swift-book
 
 print("give me a number", terminator: ": ")
+var myFloat = Float(readLine()!)!
+
+// var myNum = Int(readLine()!)!
 // let myOptStringNum = readLine()
 // let myString = readLine()!
+//
 // let myString = myOptStringNum!
 // let myNum1 = Int(myOptStringNum!)!
 // let myNum2 = Int(myString)!
 // print(myString)
 // print(myNum1 * 42)
-// var myNum = Int(readLine()!)!
-var myFloat = Float(readLine()!)!
 // print(myFloat * 42)
 var myProduct = myFloat * 42
 print("\(myFloat) * 42 = \(myProduct)")
